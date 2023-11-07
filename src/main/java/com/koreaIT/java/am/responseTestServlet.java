78e4601d42fe8195cca41eed24f66833f0f92bb0
jeongo0 <jeongo2023911@gamil.com>
2023-11-07 21:53:12 +0900

    Servlet이 jsp에게 요청을 넘겨서 jsp가 응답

--- src/main/java/com/koreaIT/java/am/responseTestServlet.java
+++ src/main/java/com/koreaIT/java/am/responseTestServlet.java
@@ -8,12 +8,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/gugudan")
-public class gugudanServlet extends HttpServlet {
+@WebServlet("/home/responseTest")
+public class responseTestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("jsp/home/gugudan.jsp").forward(request, response);
+		request.getRequestDispatcher("/jsp/home/repoonseTest.jsp").forward(request, response);
 	}
-	
+
 }
