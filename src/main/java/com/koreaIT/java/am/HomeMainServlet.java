a553292a15d62f76903f6035b69612bc9cf53a9b
jeongo0 <jeongo2023911@gamil.com>
2023-11-08 07:59:30 +0900

    article detail, main

--- src/main/java/com/koreaIT/java/am/HomeMainServlet.java
+++ src/main/java/com/koreaIT/java/am/HomeMainServlet.java
@@ -13,7 +13,7 @@ public class HomeMainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ");
+		request.getRequestDispatcher("/jsp/home/main.jsp").forward(request, response);
 	}
 
 }
